bd332b55fd14dcc08c8ae2836f110071abcf4138
Robert Morain <robert.morain@gmail.com>
2022-11-18 15:08:18 -0700

    Added embed function

--- ridley/document_embeddings.py
+++ ridley/document_embeddings.py
@@ -1,19 +1,51 @@
 import torch
-from transformers import RealmScorer, RealmTokenizer
+from transformers import RealmEmbedder, RealmScorer, RealmTokenizer
 
 
-def score():
-    tokenizer = RealmTokenizer.from_pretrained("google/realm-cc-news-pretrained-scorer")
-    model = RealmScorer.from_pretrained(
-        "google/realm-cc-news-pretrained-scorer", num_candidates=2
+def score(model, tokenizer, input_texts, candidate_texts):
+    inputs = tokenizer(input_texts, return_tensors="pt", padding=True)
+    candidate_inputs = tokenizer.batch_encode_candidates(
+        candidate_texts, max_length=10, return_tensors="pt"
     )
+    outputs = model(
+        **inputs,
+        candidate_input_ids=candidate_inputs.input_ids,
+        candidate_attention_mask=candidate_inputs.attention_mask,
+        candidate_token_type_ids=candidate_inputs.token_type_ids,
+    )
+    return outputs
 
-    # batch_size = 2, num_candidates = 2
-    input_texts = ["How are you?", "What is the item in the picture?"]
-    candidate_texts = [
-        ["Hello world!", "Nice to meet you!"],
-        ["A cute cat.", "An adorable dog"],
-    ]
 
-    inputs = tokenizer(input_texts, return_tensors="pt")
-    print(inputs)
+def embed(embedder, tokenizer, input_texts):
+    inputs = tokenizer(input_texts, return_tensors="pt", padding=True)
+    result = embedder(
+        **inputs,
+        output_hidden_states=True,
+    )
+    return result
+
+
+scorer = RealmScorer.from_pretrained(
+    "google/realm-cc-news-pretrained-scorer", num_candidates=2
+)
+embedder = RealmEmbedder.from_pretrained("google/realm-cc-news-pretrained-scorer")
+tokenizer = RealmTokenizer.from_pretrained("google/realm-cc-news-pretrained-scorer")
+# input_texts = ["How are you?", "What is the item in the picture?"]
+input_texts = [
+    "San Jose, officially San José, is a major city in the U.S. state of California that is the cultural, financial, and political center of Silicon Valley and largest city in Northern California by both population and area.",
+    "The theory of relativity usually encompasses two interrelated theories by Albert Einstein: special relativity and general relativity, proposed and published in 1905 and 1915, respectively.",
+]
+# input_texts = ["How are you?"]
+candidate_texts = [
+    ["Hello world!", "Nice to meet you!"],
+    ["A cute cat.", "An adorable dog"],
+]
+
+# result = score(model, tokenizer, input_texts, candidate_texts)
+result = embed(embedder, tokenizer, input_texts)
+cos = torch.nn.CosineSimilarity(dim=0)
+E = result.hidden_states[-1]
+e1 = E[0, 0]
+e2 = E[1, 0]
+sim = cos(e1, e2)
+print(sim)
--- tests/test_document_embeddings.py
+++ tests/test_document_embeddings.py
@@ -1,21 +1,32 @@
 import unittest
 
 import torch
+from ridley.document_embeddings import score
 from transformers import RealmScorer, RealmTokenizer
 
 
 class TestDocumentEmbeddings(unittest.TestCase):
     def setUp(self):
+        self.model = RealmScorer.from_pretrained(
+            "google/realm-cc-news-pretrained-scorer", num_candidates=2
+        )
         self.tokenizer = RealmTokenizer.from_pretrained(
             "google/realm-cc-news-pretrained-scorer"
         )
         self.input_texts = ["How are you?", "What is the item in the picture?"]
+        self.candidate_texts = [
+            ["Hello world!", "Nice to meet you!"],
+            ["A cute cat.", "An adorable dog"],
+        ]
 
     def test_tokenize(self):
         result = self.tokenizer(
             self.input_texts, return_tensors="pt", padding="longest"
         )
         self.assertIsNotNone(result)
+
+    def test_score(self):
+        score(self.model, self.tokenizer, self.input_texts, self.candidate_texts)
 
 
 if __name__ == "__main__":
