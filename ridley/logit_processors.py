e24a021cdc062a502858e90f36e2fac180d21748
Robert Morain <robert.morain@gmail.com>
2022-12-01 14:30:35 -0700

    Added rhyming logit processor

--- ridley/logit_processors.py
+++ ridley/logit_processors.py
@@ -1,9 +1,34 @@
+import requests
+import torch
 from transformers import LogitsProcessor
 
 
 class RhymeLogitsProcessor(LogitsProcessor):
-    def __init__(self):
-        pass
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
 
     def __call__(self, input_ids, scores):
+        prior = self.rhyming_prior(input_ids, scores)
+        scores = scores @ prior
         return scores
+
+    def request_rhymes(self, word):
+        response = requests.get(
+            f"https://rhymebrain.com/talk?function=getRhymes&word={word}"
+        )
+        return response
+
+    def rhyming_prior(self, input_ids, scores):
+        # Decode input_ids
+        text = self.tokenizer.decode(input_ids)
+        # Get rhyming word
+        word = text.split()[-1]
+        response = self.request_rhymes(word)
+        rhyming_words = [x["word"] for x in response.json()]
+        rhyming_tokens = self.tokenizer(rhyming_words).input_ids
+        flat_list = [item for sublist in rhyming_tokens for item in sublist]
+        mask = torch.zeros(self.tokenizer.vocab_size)
+        # Set rhyming tokens to 1
+        mask[torch.tensor(flat_list)] = 1
+        new_scores = scores * mask
+        return new_scores
--- tests/test_logit_processors.py
+++ tests/test_logit_processors.py
@@ -1,17 +1,27 @@
 import unittest
 
+import requests
+import torch
 from ridley.logit_processors import RhymeLogitsProcessor
 from ridley.riddle_generation import generate
+from transformers import GPT2Tokenizer
 
 
 class TestRhymeLogitsProcessor(unittest.TestCase):
     def setUp(self):
-        pass
-
-    def test_init(self):
-        rhyme_lp = RhymeLogitsProcessor()
-        self.assertIsInstance(rhyme_lp, RhymeLogitsProcessor)
+        self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+        self.rhyme_lp = RhymeLogitsProcessor(self.tokenizer)
 
     def test_generate(self):
         result = generate(logits_processor=[RhymeLogitsProcessor()], do_sample=True)
+        self.assertIsNotNone(result)
+
+    def test_request_rhymes(self):
+        result = self.rhyme_lp.request_rhymes("hello")
+        self.assertIsInstance(result, requests.models.Response)
+
+    def test_rhyming_prior(self):
+        input_ids = self.tokenizer("Hello world").input_ids
+        scores = torch.rand(self.tokenizer.vocab_size)
+        result = self.rhyme_lp.rhyming_prior(input_ids, scores)
         self.assertIsNotNone(result)
